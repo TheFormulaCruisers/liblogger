b7385c3c7e8de5515aec9d82e61e57fded7a10ed
Rogier Reerink <me@rogier.io>
2020-01-07 11:17:49 +0100

    Initial (unfinished) logger implementation.

--- inc/logger.h
+++ inc/logger.h
@@ -1,6 +1,53 @@
 #ifndef _LOGGER_H
 #define _LOGGER_H
 
+/**
+ * @brief
+ */
+typedef enum {
+	LOGGER_LOG_SYSTEM,
+	LOGGER_LOG_THROTTLE,
+	LOGGER_LOG_BRAKE,
+	LOGGER_LOG_WHEEL1,
+	LOGGER_LOG_WHEEL2,
+	LOGGER_LOG_MOTOR_TEMP,
+	LOGGER_LOG_FLUID_FLOW,
+	LOGGER_LOG_FLUID_TEMP
+} logger_log_t;
 
+/**
+ * @brief
+ */
+#define LOGGER_LOGSIZE 8
+
+/**
+ * @brief
+ */
+#define LOGGER_BUFSIZE LOGGER_LOGSIZE*2
+
+/**
+ * @brief
+ */
+typedef struct {
+	uint8_t r_pos;
+	uint8_t w_pos;
+	uint16_t mem[LOGGER_BUFSIZE];
+} logger_memory_t;
+
+/**
+ * @brief
+ * @param mem
+ * @return void
+ */
+void logger_init(logger_memory_t *mem);
+
+/**
+ * @brief
+ * @param mem
+ * @param type
+ * @param value
+ * @return void
+ */
+void logger_put(logger_memory_t *mem, logger_log_t type, uint16_t value);
 
 #endif // _LOGGER_H
--- src/logger.c
+++ src/logger.c
@@ -1,2 +1,26 @@
+#include <stdint.h>
 #include <logger.h>
 
+void logger_init(logger_memory_t *mem) {
+
+	int i;
+	for (i = 0; i < LOGGER_BUFSIZE; i++) {
+		mem->mem[i] = 0x7F;
+	}
+
+	mem->r_pos = 0;
+	mem->w_pos = 1;
+}
+
+void logger_put(logger_memory_t *mem, logger_log_t type, uint16_t value) {
+
+	const uint8_t w_pos = mem->w_pos;
+	const uint8_t log_pos1 = type;
+	const uint8_t log_pos2 = type + LOGGER_LOGSIZE;
+
+	if (w_pos > log_pos1 && w_pos <= log_pos2) {
+		mem->mem[log_pos2] = value;
+	} else {
+		mem->mem[log_pos1] = value;
+	}
+}
