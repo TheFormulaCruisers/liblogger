484e3ec6bc2e82e8b0de29a050fe33249d1cd7e9
Rogier Reerink <me@rogier.io>
2020-01-14 13:21:33 +0100

    Rename logger_tests to logger_test.

---
+++ tools/mplabx/build/default/debug/_ext/1445274692/logger.o.d
@@ -0,0 +1,10 @@
+build/default/debug/_ext/1445274692/logger.o.d \
+ build/default/debug/_ext/1445274692/logger.o: ../../src/logger.c \
+ /usr/microchip/xc8/v2.10/avr/lib/gcc/avr/5.4.0/include/stdint.h \
+ /usr/microchip/xc8/v2.10/avr/avr/include/stdint.h ../../inc/logger.h
+
+/usr/microchip/xc8/v2.10/avr/lib/gcc/avr/5.4.0/include/stdint.h:
+
+/usr/microchip/xc8/v2.10/avr/avr/include/stdint.h:
+
+../../inc/logger.h:
